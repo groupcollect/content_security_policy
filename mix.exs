88c3b0f9e12c79b058e7563d172eb80457f8c3b9
David Viramontes <dviramontes@gmail.com>
2025-05-16 12:30:59 -0400

    PR feedback: remove extra apps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ContentSecurityPolicy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :plug, :phoenix_live_view]
+      extra_applications: [:logger]
     ]
   end
 
